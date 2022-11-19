c8b246ee3a1bd32e3ab94328a0391d595c0009a9
seongjin <kssj9056@gmail.com>
2022-11-19 23:58:51 +0900

    쇼핑몰 셋팅

--- myPJ6_라우터/router/src/App.js
+++ myPJ6_라우터/router/src/App.js
@@ -2,11 +2,14 @@ import Homepage from "./page/Homepage";
 import About from "./page/About";
 import Product from "./page/Product";
 import ProductDetail from "./page/ProductDetail";
-import {Routes, Route} from 'react-router-dom';
-
+import {Routes, Route,Navigate} from 'react-router-dom';
+import Loginpage from "./page/Loginpage";
+import {useState} from 'react';
+import Userpage from "./page/Userpage";
 function App() {
+  const [authenticate,setAuthenticate] = useState(true);
   const Private = () => {
-      return 
+      return authenticate == true ? <Userpage/> : <Navigate to={'/login'}/>
   };
 
   return (
@@ -16,6 +19,8 @@ function App() {
           <Route path="/about" element={<About/>}/>
           <Route path="/product" element={<Product/>}/>
           <Route path="/product/:id/:num" element={<ProductDetail/>}/>
+          <Route path="/login" element={<Loginpage/>}></Route>
+          <Route path="/user" element={<Private/>}></Route>
       </Routes>
     </div>
   );
